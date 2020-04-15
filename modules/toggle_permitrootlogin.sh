95869eff8b36e475ead8fb4cf86a0ee344d88769
Jonas Pammer <privat@jonaspammer.at>
2020-04-15 18:13:30 +0200

    Actually fix is running as root itself

--- modules/toggle_permitrootlogin.sh
+++ modules/toggle_permitrootlogin.sh
@@ -13,7 +13,10 @@ call_module(){
       ;;
     "disable"|"off"|"no")
       log_info "= Checking if script got run by the ACTUAL 'root' itself..."
-      if [[ "$(echo "${HOME}")" = "/root" ]]; then
+      # See https://serverfault.com/a/568628 - when running using sudo-command, 4 new environment-variables get injected into the current context.
+      # At the start of the script we check if the script got run with superuser privileges. (= either sudo or as just when normally logged in as "root")
+      # If no SUDO_-Variables got injected, or if the injected SUDO_USER equals "root", abort.
+      if [[ ! -z "${SUDO_USER}" ]] || [[ "${SUDO_USER}" = "root" ]]; then
         log_error "Please log out of root and log into an other sudo-user! Aborting..."
         return 1
       fi
